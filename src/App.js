ae3cdfa12cef5e8d404dc18600a6c02cae8d87e5
Barak Ben Gur <barakbbg@gmail.com>
2021-08-08 19:33:49 +0300

    adding delete method

--- src/App.js
+++ src/App.js
@@ -10,23 +10,29 @@ class App extends React.Component {
 
   addNote = (val) => {
     console.log("adding a note", val);
-    this.setState({notes: [val, ...this.state.notes]})
-  }
+    this.setState({ notes: [val, ...this.state.notes] });
+  };
+
+  deleteNote = (val) => {
+    console.log("notes arr: ", this.state.notes);
+    console.log("deleting", val);
+
+    let index = this.state.notes.indexOf(val);
+    console.log("index is:", index);
+    this.setState({ notes: this.state.notes.filter((note) => note !== val) });
+  };
 
   render() {
+    console.log("notes arr: ", this.state.notes);
     return (
       <div>
         <h1>My Notes Board</h1>
         <div className="container">
-          <Input 
-          newNote={this.addNote}/>
+          <Input newNote={this.addNote} />
         </div>
         <div className="notes">
           {this.state.notes.map((note, i) => {
-            return (
-              <Note 
-              note={note}/>
-            )
+            return <Note key={i} deleteNote={this.deleteNote} note={note} />;
           })}
         </div>
       </div>
--- src/Note.js
+++ src/Note.js
@@ -2,11 +2,21 @@ import React from "react";
 import "./index.css";
 
 const Note = (props) => {
-    return (
-        <div className="note-card">
-            <div className="para">{props.note}</div>
-        </div>
-    )
-}
+
+  const deleteNote = () => {
+    console.log("deleting note");
+    console.log(props);
+    props.deleteNote(props.note)
+  };
+
+  return (
+    <div className="note-card">
+      <button className="delete-btn" onClick={deleteNote}>
+        X
+      </button>
+      <div className="para">{props.note}</div>
+    </div>
+  );
+};
 
 export default Note;
